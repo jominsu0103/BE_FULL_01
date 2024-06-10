c8465a07590a966b053117be5d29fdbcbe968544
usnimoj <jominsu0103@naver.com>
2024-06-10 18:23:30 +0900

    init & project setting

---
+++ src/main/java/com/github/be_full_01/BeFull01Application.java
@@ -0,0 +1,13 @@
+package com.github.be_full_01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeFull01Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BeFull01Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/be_full_01/BeFull01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.be_full_01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeFull01ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
